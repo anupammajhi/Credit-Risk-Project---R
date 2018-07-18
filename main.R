36ac41d33f7217f9ab8af2da10c64bd39f624e57
Anupam Majhi <majhi.anupam@gmail.com>
2018-07-18 07:27:51 +0530

    Quick Commit

--- main.R
+++ main.R
@@ -490,21 +490,3 @@ dem_melted_cormat <- melt(dem_upper_tri, na.rm = TRUE)
 
 
 ggplot(full_melted_cormat, aes(Var2, Var1, fill = value))+
-  geom_tile(color = "white")+
-  scale_fill_gradient2(low = "red", high = "green", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab") +
-  theme_minimal()+ 
-  theme(axis.text.x = element_blank())+
-  coord_fixed()+
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
