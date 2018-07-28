ce7c2a38af49f8d346339fe448fe83e3cbcdd8ba
Anupam Majhi <majhi.anupam@gmail.com>
2018-07-28 05:38:20 +0530

    Changes in code

--- main.R
+++ main.R
@@ -1951,3 +1951,62 @@ full_logistic_model_3 <- glm( Performance.Tag ~ woe.No.of.times.30.DPD.or.worse.
                                 woe.No.of.dependents.binned +
                                 woe.Outstanding.Balance.binned,
                               data = full_train_smoted,
+                              family = "binomial")
+
+summary(full_logistic_model_3)
+
+
+
+# Removing woe.No.of.trades.opened.in.last.6.months.binned
+full_logistic_model_4 <- glm( Performance.Tag ~ woe.No.of.times.30.DPD.or.worse.in.last.6.months.binned +
+                                woe.No.of.times.30.DPD.or.worse.in.last.12.months.binned +
+                                woe.Avgas.CC.Utilization.in.last.12.months.binned +
+                                woe.No.of.PL.trades.opened.in.last.12.months.binned +
+                                woe.No.of.trades.opened.in.last.12.months.binned +
+                                woe.No.of.Inquiries.in.last.12.months..excluding.home...auto.loans..binned +
+                                woe.No.of.Inquiries.in.last.6.months..excluding.home...auto.loans..binned +
+                                woe.No.of.months.in.current.company.binned +
+                                woe.Presence.of.open.home.loan.binned +
+                                woe.No.of.dependents.binned +
+                                woe.Outstanding.Balance.binned,
+                              data = full_train_smoted,
+                              family = "binomial")
+
+summary(full_logistic_model_4)
+
+
+# Removing woe.Outstanding.Balance.binned
+full_logistic_model_5 <- glm( Performance.Tag ~ woe.No.of.times.30.DPD.or.worse.in.last.6.months.binned +
+                                woe.No.of.times.30.DPD.or.worse.in.last.12.months.binned +
+                                woe.Avgas.CC.Utilization.in.last.12.months.binned +
+                                woe.No.of.PL.trades.opened.in.last.12.months.binned +
+                                woe.No.of.trades.opened.in.last.12.months.binned +
+                                woe.No.of.Inquiries.in.last.12.months..excluding.home...auto.loans..binned +
+                                woe.No.of.Inquiries.in.last.6.months..excluding.home...auto.loans..binned +
+                                woe.No.of.months.in.current.company.binned +
+                                woe.Presence.of.open.home.loan.binned +
+                                woe.No.of.dependents.binned,
+                              data = full_train_smoted,
+                              family = "binomial")
+
+summary(full_logistic_model_5)
+
+
+# Removing woe.No.of.PL.trades.opened.in.last.12.months.binned
+full_logistic_model_6 <- glm( Performance.Tag ~ woe.No.of.times.30.DPD.or.worse.in.last.6.months.binned +
+                                woe.No.of.times.30.DPD.or.worse.in.last.12.months.binned +
+                                woe.Avgas.CC.Utilization.in.last.12.months.binned +
+                                woe.No.of.trades.opened.in.last.12.months.binned +
+                                woe.No.of.Inquiries.in.last.12.months..excluding.home...auto.loans..binned +
+                                woe.No.of.Inquiries.in.last.6.months..excluding.home...auto.loans..binned +
+                                woe.No.of.months.in.current.company.binned +
+                                woe.Presence.of.open.home.loan.binned +
+                                woe.No.of.dependents.binned,
+                              data = full_train_smoted,
+                              family = "binomial")
+
+summary(full_logistic_model_6)
+
+
+# Removing woe.No.of.dependents.binned
+full_logistic_model_7 <- glm( Performance.Tag ~ woe.No.of.times.30.DPD.or.worse.in.last.6.months.binned +
