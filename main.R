a0ebc35c85168f3790d40704bb8df78fb1202251
Anupam Majhi <majhi.anupam@gmail.com>
2018-07-29 08:36:36 +0530

    Fix line

--- main.R
+++ main.R
@@ -2284,16 +2284,3 @@ lines(x=c(0,1),y=c(0,1))
 full_auc_incl_rejects <- performance(full_logistic_pred_object_test_incl_rejects, "auc")
 full_auc_incl_rejects@y.values[[1]] # 0.7688826
 
-
-# Lift and Gain Chart
-
-full_logistic_performance_measures_test_incl_rejects <- performance(full_logistic_pred_object_test_incl_rejects, "lift", "rpp")
-plot(full_logistic_performance_measures_test_incl_rejects)
-
-lift <- function(labels,predicted_prob,groups=10){
-  if(is.factor(labels)){labels <- as.integer(as.character(labels))}
-  if(is.factor(predicted_prob)){predicted_prob <- as.integer(as.character(predicted_prob))}
-  helper = data.frame(cbind(labels,predicted_prob))
-  helper[,"bucket"] = ntile(-helper[,"predicted_prob"],groups)
-  gaintable = helper %>% group_by(bucket) %>%
-    summarise_at(vars(labels ),funs(total = n(),totalresp=sum(., na.rm = TRUE))) %>%
